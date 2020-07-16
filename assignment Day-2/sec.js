8e0be405c059ab41b55417683971af214f4d4f5d
SupriyaEdururi <Supriya.edururi@gmail.com>
2020-07-15 22:57:38 -0500

    Day-2

---
+++ assignment Day-2/sec.js
@@ -0,0 +1,31 @@
+var s1="This is supriya";
+console.log(s1);
+var s2="         hi";
+//String Method's
+console.log(s1.substring(5,7));
+console.log(s1.search('i'));
+console.log(s2.trim());
+console.log(s1.toUpperCase());
+console.log(s1.toString());
+console.log(s1.toLocaleLowerCase());
+console.log(s1.toLocaleUpperCase());
+console.log(s1.slice(2,5));
+console.log(s1.startsWith('T'));
+
+//Array Method's
+var arr=['suppi','meena','pani'];
+console.log(arr[2]);
+arr[2]='sangi';
+console.log(arr);
+console.log(arr.length);
+console.log(arr.join());
+console.log(arr.pop('sangi'));
+console.log(arr.shift());
+console.log(arr.unshift('priya'));
+console.log(arr.slice(1,2));
+console.log(arr.lastIndexOf());
+console.log(arr.sort());
+console.log(arr.toString());
+var arr1=[1,2,3,4,5];
+console.log(arr1.reverse());
+console.log(arr.indexOf('meena'));
---
+++ assignment Day-2/th.js
@@ -0,0 +1,8 @@
+var val=prompt("Enter your age");
+if (val>21) {
+    console.log("can drink");//inside the console
+    document.write("can drink");
+} else {
+    console.log("cannot drink");//inside the console
+    document.write("cannot drink");
+}
